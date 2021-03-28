8acce17be39a65344933e60415ddf34960828675
minango <81500601+minango@users.noreply.github.com>
2021-03-28 14:34:23 +0900

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+def on_received_string(receivedString):
+    music.play_melody(receivedString, 120)
+radio.on_received_string(on_received_string)
+
+basic.show_icon(IconNames.EIGTH_NOTE)
 music.play_melody("C D E F G A B C5 ", 120)
+radio.set_group(1)
+radio.send_number(0)
+radio.send_string("")
 
 def on_forever():
     basic.show_leds("""
@@ -8,9 +16,6 @@ def on_forever():
         . . . # .
         . # # # .
         """)
-basic.forever(on_forever)
-
-def on_forever2():
     basic.show_leds("""
         . . # . .
         . # . # .
@@ -18,9 +23,6 @@ def on_forever2():
         . . # . .
         # # # # #
         """)
-basic.forever(on_forever2)
-
-def on_forever3():
     basic.show_leds("""
         . . # . .
         . . # . .
@@ -28,14 +30,12 @@ def on_forever3():
         . . # . .
         . . # . .
         """)
-basic.forever(on_forever3)
-
-def on_forever4():
     basic.show_leds("""
         . . # # .
         # # # . .
         . . # # #
+        . . # . .
         # # # . .
-        . . . . .
         """)
-basic.forever(on_forever4)
+    basic.show_icon(IconNames.HAPPY)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,4 @@
-input.onButtonPressed(Button.A, function () {
-    let スプライト: game.LedSprite = null
-    basic.showIcon(IconNames.Heart)
-    スプライト.turn(Direction.Right, 45)
-})
+basic.showIcon(IconNames.EigthNote)
 music.playMelody("C D E F G A B C5 ", 120)
 basic.forever(function () {
     basic.showLeds(`
@@ -30,8 +26,7 @@ basic.forever(function () {
         . . # # .
         # # # . .
         . . # # #
+        . . # . .
         # # # . .
-        . . . . .
         `)
-    basic.showIcon(IconNames.Happy)
 })
